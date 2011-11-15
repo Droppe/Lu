41cf0b6e4e4c3838031ed91e6863b9dda9d3f8de
Jim Gourgoutis <jimg@yojimg.net>
2011-11-15 10:40:04 -0800

    updates for carousel

--- public/scripts/packages/ui/Carousel.js
+++ public/scripts/packages/ui/Carousel.js
@@ -16,10 +16,43 @@ Carousel = List.extend( function ( $element, settings ){
 		};
   
 	settings = _.extend( defaults, settings );
-  
+   
+  //Since we can't call super(), we need to overwrite the List's items 
   if ( settings.items ) {
     Carousel.setItems(settings.items);
   }
+  
+  
+  /**
+   * Selects the next item in the list. 
+   * @method next
+   * @public
+   * @return {Void}
+   */
+  Carousel.next = function() {
+    if (Carousel.hasNext()) {
+      Carousel.select( Carousel.index() + 1);
+    }
+    else {
+      Carousel.first();
+    }
+  };
+
+  /**
+   * Selects the previous item in the list. 
+   * @method previous
+   * @public
+   * @return {Void}
+   */  
+   Carousel.previous = function() {
+     if (Carousel.hasPrevious()) {
+       Carousel.select( Carousel.index() - 1);
+     }
+     else {
+       Carousel.last();
+     }
+   };
+  
   
 });
 
