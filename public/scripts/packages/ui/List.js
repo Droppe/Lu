9171a6535233e613b703f8cb4f9a39daa42348ab
Robert Martone <iheartweb@gmail.com>
2011-11-18 16:07:00 -0800

    Refactor List and Carousel

--- public/scripts/packages/ui/List.js
+++ public/scripts/packages/ui/List.js
@@ -11,7 +11,7 @@ var Class = li.require( 'libraries/ptclass' ),
  */
 List =  Class.create( Abstract, ( function () {
 
-  // RETURN METHODS OBJECT 
+  //RETURN METHODS OBJECT 
   return {
     /**
      * PTClass constructor 
@@ -21,17 +21,17 @@ List =  Class.create( Abstract, ( function () {
      * @param {Object} $element JQuery object for the element wrapped by the component
      * @param {Object} settings Configuration settings
      */    
-     initialize: function ( $super, $element, settings ){
+    initialize: function ( $super, $element, settings ){
 
-       // PRIVATE INSTANCE PROPERTIES
-      
+      // PRIVATE INSTANCE PROPERTIES
+
       /**
        * Instance of Button
        * @property Button
        * @type Object
        * @private
        */  
-      var self = this,
+      var List = this,
       /**
        * Default configuration values
        * @property defaults
@@ -63,10 +63,10 @@ List =  Class.create( Abstract, ( function () {
 
       // CALL THE PARENT'S CONSTRUCTOR
       $super( $element, settings );
-      
+
       //Scan for items from the provided selector, or default to the children of the container.
       if ( settings.items ) {
-        if( typeof config.items === 'string' ) {
+        if( typeof settings.items === 'string' ) {
           $items =  $element.children( settings.items ).children();
         } else {
           $items = settings.items.children();
@@ -74,212 +74,208 @@ List =  Class.create( Abstract, ( function () {
       } else {
         $items = $element.children();
       }
-       
-      
+
       // PRIVATE METHODS
       // n/a
-      
-      
+
+
       // PRIVILEGED METHODS
+
       /**
-       * Gets JQuery collection of like items in the list
-       * @method Name
-       * @public|private
-       * @param {Type} Name Description
-       */
-      self.getItems = function () {
-        return _.clone($items);
+       * Append a new item to $element
+       * @method append
+       * @public
+       * @param {Array} A jQuery Collection of $items to append
+       * @return {Object} List
+       */  
+      List.append = function( $item ) {
+        $items.parent().append( $item );
+        return List;
       };
-      
+
+      /**
+       * Removes an item from $element
+       * @method remove
+       * @public
+       * @param {Array} A jQuery Collection of $items to remove
+       * @return {Object} List
+       */  
+      List.remove = function( $item ) {
+        $( $item, $items ).remove();
+        return List;
+      };
+      /**
+       * Select an item in the list
+       * @method select
+       * @public
+       * @param {Integer|Object} item The index of the item to select, or a JQuery instance of the item.
+       * @return {Object} List
+       */  
+      List.select = function( item ) {
+        var $item;
+
+        if( typeof item === 'number' ) {
+          $item = $items.eq( item );
+        } else {
+          $item = item;
+        }
+
+        if( $item.hasClass( settings.selectFlag ) === false ) {
+          $items.filter( '.' + settings.selectFlag ).removeClass( settings.selectFlag );
+          this.trigger( 'selected', [$item.addClass( settings.selectFlag ), this.index()] );
+        }
+        return List;
+      };
+
+      /**
+       * Selects the next item in the list. 
+       * @method next
+       * @public
+       * @return {Object} List
+       */
+      List.next = function() {
+        var increment = 0;
+        if(  List.hasNext() ) {
+          increment = 1;
+        } else {
+           List.trigger( 'max' );
+        }
+        List.select( List.getItems().eq( List.index() + increment ) );
+
+        return List;
+      };
+
+      /**
+       * Selects the previous item in the list. 
+       * @method previous
+       * @public
+       * @return {Object} List
+       */  
+      List.previous = function() {
+        var decrement = 0;
+
+        if( List.hasPrevious() ) {
+          decrement = 1;
+        } else {
+           List.trigger( 'min' );
+        }
+
+        List.select( this.getItems().eq( this.index() - decrement ) );
+        return this;
+      };
+
+      /**
+       * Selects the last item in the list. 
+       * @method last
+       * @public
+       * @return {Object} List
+       */
+      List.last = function() {
+        List.select( items.eq( List.size() - 1 ) );
+        return List;
+      };
+
+      /**
+       * Selects the first item in the list. 
+       * @method first
+       * @public
+       * @return {Object} List
+       */  
+      List.first = function() {
+        List.select( 0 );
+        return List;
+      };
+
+      /**
+       * Determines if there are any higher-index items in the list.
+       * @method hasNext
+       * @public
+       * @return {Boolean} true if not at the last item in the list
+       */
+      List.hasNext = function() {
+        return ( List.index() < List.size() - 1 );
+      };
+
+      /**
+       * Determines if there are any lower-index items in the list.
+       * @method hasPrevious
+       * @public
+       * @return {Boolean} true if not at the first item in the list
+       */
+      List.hasPrevious = function() {
+        return ( List.index() > 0 );
+      };
+
+      /**
+       * Returns the index of the selected item.
+       * @method index
+       * @public
+       * @return {Number} The index of the selected item
+       */
+      List.index = function() {
+        var ndx = -1;
+        _.each( $items, function( item, index ) {
+          var $item = $( item );
+          if( $item.hasClass( settings.selectFlag ) ) {
+            ndx = index;
+            return;
+          }
+        } );
+        return ndx;
+      };
+
+      /**
+       * Returns the currently-selected item.
+       * @method current
+       * @public
+       * @return {Object} JQuery object-reference to the selected item
+       */  
+      List.current = function() {
+        return $items.eq( List.index() );
+      };
+
+      /**
+       * Returns the array of list items. 
+       * @method items
+       * @public
+       * @return {Array} The array of list items
+       */  
+      List.items = function() {
+        return $items;
+      };
+
+      /**
+       * Returns the number of items in the list. 
+       * @method size
+       * @public
+       * @return {Number} The number of items in the list
+       */
+      List.size = function() {
+        return $items.length;
+      };
+
       // EVENT BINDINGS
       $element.on( 'select', function( event, item ) {
-         event.stopPropagation();
-         self.select( item );
-       } );
-       $element.on( 'next', function( event, item ) {
-         event.stopPropagation();
-         self.next();
-       } );
-       $element.on( 'previous', function( event, item ) {
-         event.stopPropagation();
-         self.previous();
-       } );
-       $element.on( 'first', function( event, item ) {
-         event.stopPropagation();
-         self.first();
-       } );
-       $element.on( 'last', function( event, item ) {
-         event.stopPropagation();
-         self.last();
-       } );
-
-      
-    },
-    /**
-     * Append a new item to $element
-     * @method append
-     * @public
-     * @param {Array} A jQuery Collection of $items to append
-     * @return {Object} List
-     */  
-    append: function ( $item ) {
-      this.getItems().parent().append( $item );
-      return this;
-    },
-    /**
-     * Removes an item from $element
-     * @method remove
-     * @public
-     * @param {Array} A jQuery Collection of $items to remove
-     * @return {Object} List
-     */  
-    remove: function ( $item ) {
-      $( $item, this.getItems().parent() ).remove();
-      return this;
-    },
-    /**
-     * Select an item in the list
-     * @method select
-     * @public
-     * @param {Integer|Object} item The index of the item to select, or a JQuery instance of the item.
-     * @return {Object} List
-     */  
-    select: function ( item ) {
-      var $item,
-        settings = this.getSetting(),
-        $items = this.getItems();
-
-      if( typeof item === 'number' ) {
-        $item = $items.eq( item );
-      } else {
-        $item = item;
-      }
-
-      if( $item.hasClass( settings.selectFlag ) === false ) {
-        $items.filter( '.' + settings.selectFlag ).removeClass( settings.selectFlag );
-        this.trigger( 'selected', [$item.addClass( settings.selectFlag ), this.index()] );
-      }
-      return this;
-    },
-    /**
-     * Selects the next item in the list. 
-     * @method next
-     * @public
-     * @return {Object} List
-     */
-    next: function() {
-      var increment = 0;
-      if(  this.hasNext() ) {
-        increment = 1;
-      } else {
-         this.trigger( 'max' );
-      }
-      this.select( this.getItems().eq( this.index() + increment ) );
-      
-      return this;
-    },
-    /**
-     * Selects the previous item in the list. 
-     * @method previous
-     * @public
-     * @return {Object} List
-     */  
-    previous: function() {
-      var decrement = 0;
-
-      if( this.hasPrevious() ) {
-        decrement = 1;
-      } else {
-         this.trigger( 'min' );
-      }
-      
-      this.select( this.getItems().eq( this.index() - decrement ) );
-      return this;
-    },
-    /**
-     * Selects the last item in the list. 
-     * @method last
-     * @public
-     * @return {Object} List
-     */  
-    last: function() {
-      var items = this.getItems();
-      
-      this.select( items.eq( items.length - 1 ) );
-      return this;
-    },
-    /**
-     * Selects the first item in the list. 
-     * @method first
-     * @public
-     * @return {Object} List
-     */  
-    first: function() {
-      this.select( 0 );
-      return this;
-    },    
-    /**
-     * Determines if there are any higher-index items in the list.
-     * @method hasNext
-     * @public
-     * @return {Boolean} true if not at the last item in the list
-     */
-    hasNext: function() {
-      return ( this.index() < this.getItems().length - 1);
-    },
-    /**
-     * Determines if there are any lower-index items in the list.
-     * @method hasPrevious
-     * @public
-     * @return {Boolean} true if not at the first item in the list
-     */
-    hasPrevious: function() {
-      return ( this.index() > 0 );
-    },
-    /**
-     * Returns the index of the selected item.
-     * @method index
-     * @public
-     * @return {Number} The index of the selected item
-     */
-    index: function() {
-      var that = this,
-        ndx = -1;
-      _.each( that.getItems(), function( item, index ) {
-        var $item = $( item );
-        if( $item.hasClass( that.getSetting().selectFlag ) ) {
-          ndx = index;
-          return;
-        }
+        event.stopPropagation();
+        List.select( item );
       } );
-      return ndx;
-    },
-    /**
-     * Returns the currently-selected item.
-     * @method current
-     * @public
-     * @return {Object} JQuery object-reference to the selected item
-     */  
-    current: function() {
-      return this.getItems().eq( this.index() );
-    },
-    /**
-     * Returns the array of list items. 
-     * @method items
-     * @public
-     * @return {Array} The array of list items
-     */  
-    items: function() {
-      return this.getItems();
-    },
-    /**
-     * Returns the number of items in the list. 
-     * @method size
-     * @public
-     * @return {Number} The number of items in the list
-     */
-    size: function() {
-      return this.getItems().length;
+      $element.on( 'next', function( event, item ) {
+        event.stopPropagation();
+        List.next();
+      } );
+      $element.on( 'previous', function( event, item ) {
+        event.stopPropagation();
+        List.previous();
+      } );
+      $element.on( 'first', function( event, item ) {
+        event.stopPropagation();
+        List.first();
+      } );
+      $element.on( 'last', function( event, item ) {
+        event.stopPropagation();
+        List.last();
+      } );
+
     }
   };
 
