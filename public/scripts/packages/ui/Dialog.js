3901b05e34a33872db790fa788fdf6f6a3c347d2
Peter Frueh <pfrueh@linkedin.com>
2011-11-28 12:03:42 -0800

    moved dialog example from in_arbys to AthenaUI (no inject yet)

---
+++ public/scripts/packages/ui/Dialog.js
@@ -0,0 +1,61 @@
+var Class = require( '/scripts/libraries/ptclass' ),
+  Abstract = require( 'ui/Abstract' ),
+  Dialog;
+
+/**
+ * Representation of a button element preconfigured with a 'next' event
+ * @class NextButton
+ * @constructor
+ * @extends Button
+ * @param {HTMLElement} element The HTML element surrounded by the control
+ * @param {Object} settings Configuration properties for this instance
+ */
+Dialog = Class.create( Abstract,  ( function () {
+
+   // RETURN METHODS OBJECT
+   return {
+     /**
+      * PTClass constructor 
+      * @method initialize
+      * @public
+      * @param {Object} $super Pointer to superclass constructor
+      * @param {Object} $element JQuery object for the element wrapped by the component
+      * @param {Object} settings Configuration settings
+      */    
+     initialize: function ( $super, $element, settings ) {
+
+       // PRIVATE INSTANCE PROPERTIES
+
+       /**
+        * Default configuration values
+        * @property defaults
+        * @type Object
+        * @private
+        * @final
+        */
+       var defaults = {
+         autoOpen: false
+       };
+       
+       // MIX THE DEFAULTS INTO THE SETTINGS VALUES
+       _.defaults( settings, defaults );
+
+       // CALL THE PARENT'S CONSTRUCTOR
+       $super( $element, settings );
+
+     	 $element.dialog(settings);
+     	 $element.on('open', function() {
+     	   $(this).dialog('open');
+       });
+    	 $element.on('close', function() {
+         $(this).dialog('close');
+       });
+     }
+  };
+  
+}() ));
+
+//Export to CommonJS Loader
+if( module && module.exports ) {
+  module.exports = Dialog;
+}
