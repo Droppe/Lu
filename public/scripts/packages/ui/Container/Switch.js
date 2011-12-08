0b97d53429c0f8896c9fa24a07e7cd23c6aac038
Jim Gourgoutis <jimg@yojimg.net>
2011-12-08 15:03:55 -0800

    switch based on two HTML buttons

--- public/scripts/packages/ui/Container/Switch.js
+++ public/scripts/packages/ui/Container/Switch.js
@@ -12,107 +12,99 @@ var Class = require( '/scripts/libraries/ptclass' ),
  */
 Switch = Class.create( Container,  ( function () {
 
-   // RETURN METHODS OBJECT
-   return {
+  var DISABLED = "disabled";
+
+  // RETURN METHODS OBJECT
+  return {
+    /**
+     * PTClass constructor 
+     * @method initialize
+     * @public
+     * @param {Object} $super Pointer to superclass constructor
+     * @param {Object} $element JQuery object for the element wrapped by the component
+     * @param {Object} settings Configuration settings
+     */    
+    initialize: function ( $super, $element, settings ) {
+
+      // PRIVATE INSTANCE PROPERTIES
+
+      /**
+       * Instance of Switch
+       * @property Switch
+       * @type Object
+       * @private
+       */     
+      var Switch = this,
+
+      /**
+       * Default configuration values
+       * @property defaults
+       * @type Object
+       * @private
+       * @final
+       */
+      defaults = {
+        buttonTag: "button",
+        on: "click select change",
+        action: "switch",
+        className: "athena-switch"
+      },
+      /**
+       * An array of objects representing available states f
+       * or the switch instance, passed on through the published event.
+       * @property states
+       * @type Array
+       */
+      states = [],
+      /**
+       * The buttons within the Switch container
+       * @property buttons
+       * @type Object
+       */
+      $buttons =[];
      
-     /**
-      * PTClass constructor 
-      * @method initialize
-      * @public
-      * @param {Object} $super Pointer to superclass constructor
-      * @param {Object} $element JQuery object for the element wrapped by the component
-      * @param {Object} settings Configuration settings
-      */    
-     initialize: function ( $super, $element, settings ) {
+      // MIX THE DEFAULTS INTO THE SETTINGS VALUES
+      _.defaults( settings, defaults );
 
-       // PRIVATE INSTANCE PROPERTIES
+      // CALL THE PARENT'S CONSTRUCTOR
+      $super( $element, settings );
+      
+      states = settings.states;
 
-       /**
-        * Instance of Switch
-        * @property Switch
-        * @type Object
-        * @private
-        */     
-       var Switch = this,
+      /**
+       * Sets the state of the switch 
+       * @method toggleState
+       * @public
+       * @param {Object} button The button that was clicked
+       * @return {Object} The Switch instance (for chaining)
+       */
+      Switch.toggleState = function (button) {
+        var $button = $(button),
+        index = $buttons.index($button),
+        state = (states) ? states[index] : null;
+        
+        _.log("Switch.toggleState", button, state);
+        $button.attr(DISABLED, DISABLED);
+        $buttons.not($button).removeAttr(DISABLED);
+        Switch.trigger( settings.action, [  state ] );
+        return Switch;
+      };
+      
+      Switch.init = function () {
+        $buttons = $(settings.buttonTag, $element);
+        $buttons.first().attr(DISABLED, DISABLED); 
+      };
 
-       /**
-        * Default configuration values
-        * @property defaults
-        * @type Object
-        * @private
-        * @final
-        */
-       defaults = {
-         on: 'select change',
-         states: [
-          {
-            state: "OFF",
-            label: "off",
-            className: "athena-switch-off"
-          }, 
-          {
-            state: "ON",
-            label: "on",
-            className: "athena-switch-on"
-          }
-         ]
-       },
-       /**
-        * List of states for the switch
-        * @property states
-        * @type Array
-        * @private
-        */
-       states,
-       i = 0,
-       statesLen,
-       lastState,
-       activeState;
+      Switch.init();
+      Switch.toggleState($buttons.first());
+            
+      $element.on( settings.on, settings.buttonTag, function ( event ) {
+        event.preventDefault();
+        Switch.toggleState(event.target);
+      });
        
-       // MIX THE DEFAULTS INTO THE SETTINGS VALUES
-       _.defaults( settings, defaults );
-   
-       // CALL THE PARENT'S CONSTRUCTOR
-       $super( $element, settings );
-       
-       states = settings.states;
-       statesLen = states.length;
-       activeState = states[i];
-       
-       /**
-        * Sets the state of the switch 
-        * @method toggleState
-        * @public
-        * @return {Object} the Switch instance (for chaining)
-        */       
-       Switch.toggleState = function () {
-         
-         lastState = activeState;
-         
-         // Increment counter
-         ++i;
-
-         // Reset the counter if at the end of the array
-         if ( i === statesLen ) {
-           i = 0;
-         }
-         
-         activeState = states[i];
-         
-         $element.removeClass(lastState.className).addClass(activeState.className);
-         
-         _.log("Switch", "state", activeState);
-         return Switch;
-       };
-              
-       $element.on( settings.on, function ( event ) {
-         event.preventDefault();
-         Switch.toggleState();
-       });
-       
-     }
+    }
   };
-  
 }() ));
 
 //Export to CommonJS Loader
