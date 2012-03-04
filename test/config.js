f55c88ffa95605fb8e4022965b2fef9c59ed6787
krisk <kirollos@gmail.com>
2012-03-04 15:06:50 -0800

    Added configuration point for browser list

---
+++ test/config.js
@@ -0,0 +1,5 @@
+exports.browsers = {
+	chrome: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
+	safari: '/Applications/Safari.app/Contents/MacOS/Safari',
+	firefox: '/Applications/Firefox.app/Contents/MacOS/firefox'
+};
