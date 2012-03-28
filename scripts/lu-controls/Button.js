0b892d8bd407983fc77a7cee1aff7c4ea90c8dd8
Jim Gourgoutis <jimg@yojimg.net>
2012-03-28 10:42:34 -0700

    code cleanup

--- scripts/lu-controls/Button.js
+++ scripts/lu-controls/Button.js
@@ -13,7 +13,7 @@ var Class = require( 'class' ),
 Button = Class.create( Abstract, ( function() {
 
   // CONSTANTS
-  var CLICK = 'click',
+  var CLICK_EVENT = 'click',
     DISABLED = 'disabled',
     LU_DISABLED = 'lu-disabled',
     ARIA_ROLE = 'role',
@@ -48,7 +48,7 @@ Button = Class.create( Abstract, ( function() {
          * @final
          */
         defaults = {
-          on: CLICK
+          on: CLICK_EVENT
         },
 
         /**
@@ -110,7 +110,7 @@ Button = Class.create( Abstract, ( function() {
       action = settings.action;
 
 
-      // PRIVATE METHODS
+      // === PRIVATE METHODS ===
 
       /**
        * Setups accessibility for the button.  If the button is a "link" then it will have an ARIA role of button and 
@@ -135,6 +135,8 @@ Button = Class.create( Abstract, ( function() {
           } );
         }  
       }
+
+      // === PRIVILEDGED METHODS ===
 
       /**
        * Disables the Button by adding a disabled attribute (inputs and buttons only) and the class lu-disabled
@@ -162,11 +164,10 @@ Button = Class.create( Abstract, ( function() {
         $element.removeClass( LU_DISABLED );
       };
 
-      // EVENT BINDINGS
+      // === EVENT BINDINGS ===
+      
       Button.on( settings.on, function( event ) {
         event.preventDefault();
-
-        _.info( CLICK, action );
 
         var parameters = [];
 
