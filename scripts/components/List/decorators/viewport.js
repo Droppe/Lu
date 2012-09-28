a41bd32ba9fc09916c4a75883f51123c878c2f92
Yi-Fan Zhang <yzhang@yzhang-mn.linkedin.biz>
2012-09-28 15:28:09 -0700

    Fixed code formatting

--- scripts/components/List/decorators/viewport.js
+++ scripts/components/List/decorators/viewport.js
@@ -1,50 +1,48 @@
-var constants = require( 'lu/constants' ),
-  Carousel = require( 'lu/Carousel' );
+var constants = require('lu/constants'),
+  Carousel = require('lu/Carousel');
 
 /**
  * @method first
  * @private
  */
-function viewportDecorator( settings ) {
+function viewportDecorator(settings) {
 
-  return function( base ){
-    var self = this;
-    var slidingWindow = this.$element;
-    var windowStart = 0;
-    var pageSize = settings.viewport.pageSize || 1;
-    var windowStartIndex = 0;
-    var threshold = Math.floor(pageSize*(settings.viewport.threshold || 1));
-    var mode = settings.viewport.mode || 'paging';
-    var previewSize = settings.viewport.previewSize || '0';
-    
-    $(slidingWindow).width(getPageWidth() + 2*previewSize*getItemWidth());
+  return function (base) {
+    var self = this,
+      slidingWindow = this.$element,
+      windowStart = 0,
+      pageSize = settings.viewport.pageSize || 1,
+      windowStartIndex = 0,
+      threshold = Math.floor(pageSize * (settings.viewport.threshold || 1)),
+      mode = settings.viewport.mode || 'paging',
+      previewSize = settings.viewport.previewSize || '0';
+
+    $(slidingWindow).width(getPageWidth() + 2 * previewSize * getItemWidth());
 
     function pageRight() {
-      var currentIndex = self.index();
-      var size = self.size();
-      var nextPageIndex = currentIndex + pageSize;
+      var currentIndex = self.index(),
+        size = self.size(),
+        nextPageIndex = currentIndex + pageSize;
 
-      if(nextPageIndex === size) {
-          nextPageIndex = 0;
+      if (nextPageIndex === size) {
+        nextPageIndex = 0;
+      } else if (size - nextPageIndex < pageSize) {
+        nextPageIndex -= pageSize - (size - nextPageIndex);
       }
-      else if(size - nextPageIndex < pageSize) {
-          nextPageIndex -= pageSize - (size - nextPageIndex);
-      }
- 
+
       self.select(nextPageIndex);
       slideToIndex(nextPageIndex);
     }
 
     function pageLeft() {
-      var currentIndex = self.index();
-      var size = self.size();
-      var prevPageIndex = currentIndex - pageSize;
-      
-      if(prevPageIndex + pageSize === 0) {
-          prevPageIndex = size - pageSize;
-      }      
-      else if(prevPageIndex < 0) {
-          prevPageIndex = 0;
+      var currentIndex = self.index(),
+        size = self.size(),
+        prevPageIndex = currentIndex - pageSize;
+
+      if (prevPageIndex + pageSize === 0) {
+        prevPageIndex = size - pageSize;
+      } else if (prevPageIndex < 0) {
+        prevPageIndex = 0;
 
       }
 
@@ -53,74 +51,70 @@ function viewportDecorator( settings ) {
     }
 
     function slideToSelected() {
-      var currentIndex = self.index();
-      var midpoint = Math.floor(windowStartIndex + pageSize/2);
+      var currentIndex = self.index(),
+        midpoint = Math.floor(windowStartIndex + pageSize / 2);
 
-        if(currentIndex === 0) {
-          windowStartIndex = 0;
-        }
-        else if(currentIndex === self.size() - 1) {
-          windowStartIndex = self.size() - pageSize;
-        }
-        else {
-          if(currentIndex >= windowStartIndex + threshold) {
-            windowStartIndex += Math.abs(currentIndex - midpoint);
+      if (currentIndex === 0) {
+        windowStartIndex = 0;
+      } else if (currentIndex === self.size() - 1) {
+        windowStartIndex = self.size() - pageSize;
+      } else {
+        if (currentIndex >= windowStartIndex + threshold) {
+          windowStartIndex += Math.abs(currentIndex - midpoint);
 
-            if(windowStartIndex > self.size() - pageSize) {
-              windowStartIndex = self.size() - pageSize;
-            }
+          if (windowStartIndex > self.size() - pageSize) {
+            windowStartIndex = self.size() - pageSize;
           }
-          else if(currentIndex < windowStartIndex + pageSize - threshold) {
-            windowStartIndex -= Math.abs(currentIndex - midpoint)
+        } else if (currentIndex < windowStartIndex + pageSize - threshold) {
+          windowStartIndex -= Math.abs(currentIndex - midpoint);
 
-            if(windowStartIndex < 0) {
-              windowStartIndex = 0;
-            }
+          if (windowStartIndex < 0) {
+            windowStartIndex = 0;
           }
-          else
-            return;
+        } else {
+          return;
         }
+      }
 
-        slideToIndex(windowStartIndex);
+      slideToIndex(windowStartIndex);
     }
-    
+
     function getPageWidth() {
-      var pageWidth = getItemWidth() * pageSize;
-      return pageWidth;
+      return getItemWidth() * pageSize;
     }
-    
+
     function getItemWidth() {
       return slidingWindow.find('li').outerWidth(true);
     }
-    
+
     function slideToIndex(index) {
       if (index === 0) {
-        slidingWindow.find('ul').animate({right: index*getItemWidth()}, 500);
+        slidingWindow.find('ul').animate({right: index * getItemWidth()}, 500);
       } else if (index >= self.size() - pageSize) {
-        slidingWindow.find('ul').animate({right: index*getItemWidth() - 2*previewSize*getItemWidth()}, 500);
+        slidingWindow.find('ul').animate({right: index * getItemWidth() - 2 * previewSize * getItemWidth()}, 500);
       } else {
-        slidingWindow.find('ul').animate({right: index*getItemWidth() - previewSize*getItemWidth()}, 500);
+        slidingWindow.find('ul').animate({right: index * getItemWidth() - previewSize * getItemWidth()}, 500);
 
       }
     }
 
-    self.on( constants.events.SELECTED, function( event, Component ){
+    self.on(constants.events.SELECTED, function (event, Component) {
       event.stopPropagation();
-      if(mode === "sliding") {
+      if (mode === "sliding") {
         slideToSelected();
       }
-    } ); 
+    });
 
-    if(mode === "paging") {
-      self.next = function() {
+    if (mode === "paging") {
+      self.next = function () {
         pageRight();
       };
-      self.previous = function() {
+      self.previous = function () {
         pageLeft();
       };
       if (!(self instanceof Carousel)) {
-        self.hasNext = function() {
-          return ( this.index() + pageSize < this.size() ); 
+        self.hasNext = function () {
+          return (this.index() + pageSize < this.size());
         };
       }
     }
@@ -129,10 +123,10 @@ function viewportDecorator( settings ) {
 }
 
 //Export to Common JS Loader
-if( typeof module !== 'undefined' ){
-  if( typeof module.setExports === 'function' ){
-    module.setExports( viewportDecorator );
-  } else if( module.exports ){
+if (typeof module !== 'undefined') {
+  if (typeof module.setExports === 'function') {
+    module.setExports(viewportDecorator);
+  } else if (module.exports) {
     module.exports = viewportDecorator;
   }
 }
