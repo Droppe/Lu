d410fc29a655de942371ae136111bcf2e9f2bdb7
Jim Gourgoutis <jimg@yojimg.net>
2012-10-17 23:02:20 -0700

    replacing lost file

--- scripts/mappers.js
+++ scripts/mappers.js
@@ -1,24 +1,160 @@
-/**
- * Lu version 0.3.2
- * @author Robert Martone <iheartweb@gmail.com>
- * @license
- *
- * Please thank the contributors:
- * https://github.com/linkedin/Lu/graphs/contributors
- *
- * Copyright (c) 2011,2012 LinkedIn
- * All Rights Reserved. Apache Software License 2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-(function(){Lu.Mapper=function(){var e=window.LU_CONFIG.scope||document;this.$scope=undefined,this.maps=[],this.setScope(e)},Lu.Mapper.prototype.setScope=function(e){function n(e,t){var n=e.getAttribute("data-lu");return n!==null&&n!==""}var t;return e instanceof $?t=e.find("[data-lu]").add(_.filter(e,n)):e.getElementsByTagName?t=$(_.filter(e.getElementsByTagName("*"),n)):(t=$(e),t=t.find("[data-lu]").add(_.filter(t,n))),this.$scope=t,this},Lu.Mapper.prototype.getScope=function(){return this.$scope},Lu.Mapper.prototype.register=function(e){return this.maps.unshift(e),this},Lu.Mapper.prototype.execute=function(){_.each(this.maps,function(e,t){e.call()})};var e=new Lu.Mapper;_.each(["Switch","List","Carousel","Container","Tip","Button"],function(t,n){e.register(function(){Lu.map(_.filter(e.$scope,function(e,n){return e.getAttribute("data-lu").indexOf(t)>-1}),t)})}),_.each(["select","first","last","next","previous","load","play","pause","state"],function(t){var n=_.filter(e.$scope,function(e){var t=e.nodeName;return t==="BUTTON"||t==="A"||t==="INPUT"});e.register(function(){var e="Button:"+t.charAt(0).toUpperCase()+t.substring(1);Lu.map(_.filter(n,function(t){return t.getAttribute("data-lu").indexOf(e)>-1}),"Button",function(){this.settings.action=t,this.key=e,this.hasDependencies=!0})})});var t=_.filter(e.$scope,function(e){return e.getAttribute("data-lu").indexOf("Tip")>-1});e.register(function(){var e="Tip";Lu.map(_.filter(t,function(t){return _.indexOf(t.getAttribute("data-lu").split(" "),e)>-1}),"Tip",function(){this.settings.placement="Right",this.key=e,this.hasDependencies=!0})}),_.each(["Above","Below","Left","Right"],function(n){e.register(function(){var e="Tip:"+n;Lu.map(_.filter(t,function(t){return t.getAttribute("data-lu").indexOf(e)>-1}),"Tip",function(){this.settings.placement=n,this.key=e,this.hasDependencies=!0})})}),e.register(function(){Lu.map(_.filter(e.$scope,function(e,t){return e.getAttribute("data-lu").indexOf("Placeholder")>-1&&(e.nodeName==="INPUT"||e.nodeName==="TEXTAREA")&&e.getAttribute("placeholder")}),"Placeholder")}),e.execute()})();
+( function(){
+
+  var Mapper,
+    tips;
+
+  /**
+   * Mappers are used to store a scope of a DOM tree
+   * to map and a registry of mappers to execute
+   * @class Mapper
+   * @constructor
+   */
+  Lu.Mapper = function(){
+    var scope = window.LU_CONFIG.scope || document;
+    this.$scope = undefined;
+    this.maps = [];
+    this.setScope( scope );
+  };
+
+  /**
+   * Sets the scope to use when mapping components to nodes
+   * @public
+   * @method setScope
+   * @param {*} scope This can be an HTML Element,
+   * an array of elements, a selector string or a jquery object
+   * @return Mapper instance
+   */
+  Lu.Mapper.prototype.setScope = function( scope ){
+    var $scope;
+
+    function filter( item, index ){
+      var lu = item.getAttribute( 'data-lu' );
+      return ( lu !== null && lu !== '' );
+    }
+
+    if( scope instanceof $ ){
+      $scope = scope.find( '[data-lu]' ).add( _.filter( scope, filter ) );
+    } else if( scope.getElementsByTagName ){
+      $scope = $( _.filter( scope.getElementsByTagName( '*' ), filter ) );
+    } else {
+      $scope = $( scope );
+      $scope = $scope.find( '[data-lu]' ).add( _.filter( $scope, filter ) );
+    }
+
+    this.$scope = $scope;
+    return this;
+  };
+
+  /**
+   * Gets a jquery object containing all Lu decorated nodes
+   * @public
+   * @method getScope
+   * @return a jquery object
+   */
+  Lu.Mapper.prototype.getScope = function(){
+    return this.$scope;
+  };
+
+  /**
+   * Registers a mapper to be executed later
+   * @public
+   * @method register
+   * @param {Function} map A function mapper to be called
+   * @return Mapper instance
+   */
+  Lu.Mapper.prototype.register = function( map ){
+    //this.maps.unshift( map );
+    this.maps.push( map );
+
+    return this;
+  };
+
+  /**
+   * Executes all registered maps
+   * @public
+   * @method execute
+   * @return Mapper instance
+   */
+  Lu.Mapper.prototype.execute = function(){
+    _.each( this.maps, function( item, index ){
+      item.call();
+    } );
+    return this;
+  };
+
+  //Create a new Mapper to contain default mappings for Lu.
+  Mapper = new Lu.Mapper();
+
+  //Generic Mappers
+  _.each( ['Switch', 'List', 'Carousel', 'Container', 'Tip', 'Button'], function( id, index ){
+    Mapper.register( function(){
+      Lu.map( _.filter( Mapper.$scope, function( item, index ){
+        return ( item.getAttribute( 'data-lu' ).indexOf( id ) > -1 );
+      } ), id );
+    } );
+  } );
+
+  // Coalesce the buttons into one mapper function, because we care about performance,
+  // saving bytes, and abhor redundancy.
+  _.each( ['select', 'first', 'last', 'next', 'previous', 'load', 'play', 'pause', 'state'], function( action ) {
+
+    var scope = _.filter( Mapper.$scope, function( item ){
+      var nodeName = item.nodeName;
+      return ( nodeName === 'BUTTON' || nodeName === 'A' || nodeName === 'INPUT' );
+    } );
+
+    Mapper.register( function(){
+      var key = 'Button:' + action.charAt( 0 ).toUpperCase() + action.substring( 1 );
+      Lu.map( _.filter( scope, function( item ){
+        return ( item.getAttribute( 'data-lu' ).indexOf( key ) > -1 );
+      } ), 'Button', function(){
+        this.settings.action = action;
+        this.key = key;
+        this.hasDependencies = true;
+      } );
+    } );
+  } );
+
+  // Tip (tooltip)
+  tips = _.filter( Mapper.$scope, function( item ){
+    return ( item.getAttribute( 'data-lu' ).indexOf( 'Tip' ) > -1 );
+  } );
+
+  Mapper.register( function(){
+    var key = 'Tip';
+    Lu.map( _.filter( tips, function( item ){
+      return ( _.indexOf( item.getAttribute( 'data-lu' ).split( ' ' ), key ) > -1 );
+    } ), 'Tip', function(){
+      this.settings.placement = 'Right';
+      this.key = key;
+      this.hasDependencies = true;
+    } );
+  } );
+
+  _.each( ['Above', 'Below', 'Left', 'Right'], function( placement ) {
+
+    Mapper.register( function(){
+      var key = 'Tip:' + placement;
+      Lu.map( _.filter( tips, function( item ){
+        return ( item.getAttribute( 'data-lu' ).indexOf( key ) > -1 );
+      } ), 'Tip', function(){
+        this.settings.placement = placement;
+        this.key = key;
+        this.hasDependencies = true;
+      } );
+    } );
+  } );
+
+  //Placeholder
+  Mapper.register( function(){
+    Lu.map( _.filter( Mapper.$scope, function( item, index ){
+      return ( item.getAttribute( 'data-lu' ).indexOf( 'Placeholder' ) > -1 &&
+             ( item.nodeName === 'INPUT' || item.nodeName === 'TEXTAREA' ) &&
+             item.getAttribute( 'placeholder' ) );
+    } ), 'Placeholder' );
+  } );
+
+  //Execute Default Mappers
+  Mapper.execute();
+
+}() );
