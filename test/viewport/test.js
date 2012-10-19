1a29ec90933581fee2948c88dd57a4c5df10acd0
Yi-Fan Zhang <yzhang@jrichard-2-mn.linkedin.biz>
2012-10-19 14:38:01 -0700

    Added mapper config for List/Carousel Viewport decorator with default config and hasDependencies. Fixed unit test accordingly.

--- test/viewport/test.js
+++ test/viewport/test.js
@@ -1,68 +1,76 @@
 function execute(){
-  var PagingList = $( '#list-paging' ).lu( 'getComponent', 'List' ).instance,
-    SlidingList = $( '#list-sliding' ).lu( 'getComponent', 'List' ).instance,
-    PagingCarousel = $( '#carousel-paging' ).lu( 'getComponent', 'Carousel' ).instance,
-    SlidingCarousel = $( '#carousel-sliding' ).lu( 'getComponent', 'Carousel' ).instance;
+  var PagingList = $( '#list-paging' ).lu( 'getComponent', 'List' ),
+    SlidingList = $( '#list-sliding' ).lu( 'getComponent', 'List' ),
+    PagingCarousel = $( '#carousel-paging' ).lu( 'getComponent', 'Carousel' ),
+    SlidingCarousel = $( '#carousel-sliding' ).lu( 'getComponent', 'Carousel' );
 
   QUnit.module( 'List Viewport Decorator Test' );
 
   QUnit.asyncTest( 'paging', function(){
-    PagingList.trigger('next');
-    ok(PagingList.index() === 5);
-    PagingList.trigger('next');
-    ok(PagingList.index() === 9);
-    ok(PagingList.hasNext() === false);
-    PagingList.trigger('previous');
-    ok(PagingList.index() === 4);
-    PagingList.trigger('previous');
-    ok(PagingList.index() === 0);
-    ok(PagingList.hasPrevious() === false);
-    start();
+    PagingList.ready(function () {
+      PagingList.instance.trigger('next');
+      ok(PagingList.instance.index() === 5);
+      PagingList.instance.trigger('next');
+      ok(PagingList.instance.index() === 9);
+      ok(PagingList.instance.hasNext() === false);
+      PagingList.instance.trigger('previous');
+      ok(PagingList.instance.index() === 4);
+      PagingList.instance.trigger('previous');
+      ok(PagingList.instance.index() === 0);
+      ok(PagingList.instance.hasPrevious() === false);
+      start();
+    });
   } );
 
   QUnit.asyncTest( 'sliding', function(){
-    ok(SlidingList.hasPrevious() === false);
-    SlidingList.trigger('next');
-    ok(SlidingList.index() === 1);
-    SlidingList.select(4);
-    ok(SlidingList.index() === 4);
-    SlidingList.select(13);
-    ok(SlidingList.hasNext() === false);
-    SlidingList.trigger('previous');
-    ok(SlidingList.index() === 12);
-    ok(SlidingList.hasNext() === true);
-    ok(SlidingList.hasPrevious() === true);
-    start();
+    SlidingList.ready(function () {
+      ok(SlidingList.instance.hasPrevious() === false);
+      SlidingList.instance.trigger('next');
+      ok(SlidingList.instance.index() === 1);
+      SlidingList.instance.select(4);
+      ok(SlidingList.instance.index() === 4);
+      SlidingList.instance.select(13);
+      ok(SlidingList.instance.hasNext() === false);
+      SlidingList.instance.trigger('previous');
+      ok(SlidingList.instance.index() === 12);
+      ok(SlidingList.instance.hasNext() === true);
+      ok(SlidingList.instance.hasPrevious() === true);
+      start();
+    });
   } );
 
   QUnit.module( 'Carousel Viewport Decorator Test' );
 
   QUnit.asyncTest( 'paging', function(){
-    PagingCarousel.trigger('next');
-    ok(PagingCarousel.index() === 5);
-    PagingCarousel.trigger('next');
-    ok(PagingCarousel.index() === 9);
-    ok(PagingCarousel.hasNext() === true);
-    PagingCarousel.trigger('next');
-    ok(PagingCarousel.index() === 0);
-    ok(PagingCarousel.hasPrevious() === true);
-    PagingCarousel.trigger('previous');
-    ok(PagingCarousel.index() === 9);
-    start();
+    PagingCarousel.ready(function() {
+      PagingCarousel.instance.trigger('next');
+      ok(PagingCarousel.instance.index() === 5);
+      PagingCarousel.instance.trigger('next');
+      ok(PagingCarousel.instance.index() === 9);
+      ok(PagingCarousel.instance.hasNext() === true);
+      PagingCarousel.instance.trigger('next');
+      ok(PagingCarousel.instance.index() === 0);
+      ok(PagingCarousel.instance.hasPrevious() === true);
+      PagingCarousel.instance.trigger('previous');
+      ok(PagingCarousel.instance.index() === 9);
+      start();
+    });
   } );
 
   QUnit.asyncTest( 'sliding', function(){
-    SlidingCarousel.trigger('next');
-    ok(SlidingCarousel.index() === 1);
-    SlidingCarousel.select(4);
-    ok(SlidingCarousel.index() === 4);
-    SlidingCarousel.select(13);
-    ok(SlidingCarousel.hasNext() === true);
-    SlidingCarousel.trigger('next');
-    ok(SlidingCarousel.index() === 0);
-    ok(SlidingCarousel.hasPrevious() === true);
-    SlidingCarousel.trigger('previous');
-    ok(SlidingCarousel.index() == 13);
-    start();
+    SlidingCarousel.ready(function() {
+      SlidingCarousel.instance.trigger('next');
+      ok(SlidingCarousel.instance.index() === 1);
+      SlidingCarousel.instance.select(4);
+      ok(SlidingCarousel.instance.index() === 4);
+      SlidingCarousel.instance.select(13);
+      ok(SlidingCarousel.instance.hasNext() === true);
+      SlidingCarousel.instance.trigger('next');
+      ok(SlidingCarousel.instance.index() === 0);
+      ok(SlidingCarousel.instance.hasPrevious() === true);
+      SlidingCarousel.instance.trigger('previous');
+      ok(SlidingCarousel.instance.index() == 13);
+      start();
+    });
   } );
 }
