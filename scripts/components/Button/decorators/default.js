64bfb021a368ac53e6c93a2a04d1dfd26b3858ee
Robert Martone <iheartweb@gmail.com>
2012-10-30 11:07:45 -0700

    merge default decorator for button

--- scripts/components/Button/decorators/default.js
+++ scripts/components/Button/decorators/default.js
@@ -8,14 +8,10 @@
 function DefaultDecorator( settings ){
   return function( base ){
     var self = this;
-<<<<<<< HEAD
     this.$element.on( settings.on, _.throttle( function( event ){
       if( settings.preventDefault ){
         event.preventDefault();
       }
-=======
-    self.$element.on( settings.on, _.throttle( function( event ){
->>>>>>> 47eca0de395b18d4db5c2aed481b7608a645c5cd
       if( !self.$element.is( 'button' ) ){
         self.$element.focus();
       }
