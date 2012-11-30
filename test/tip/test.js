30650d656d7b09bfd8e670920a2ce48c75c46e14
Brendan Dean <brendean@gmail.com>
2012-11-30 14:46:47 -0800

    correcting and adding some additional tooltip tests

--- test/tip/test.js
+++ test/tip/test.js
@@ -2,38 +2,92 @@ function execute(){
   var mouseTipEl = $( '#mouse-tip' ),
       mouseTipComponent = mouseTipEl.lu( 'getComponent', 'Tip' ),
       inputTipEl = $( '#input-tip' ),
-      inputTipComponent = inputTipEl.lu( 'getComponent', 'Tip' );
+      inputTipComponent = inputTipEl.lu( 'getComponent', 'Tip' ),
+      persistentTipEl = $( '#persistent-tip' ),
+      persistentTipComponent = persistentTipEl.lu( 'getComponent', 'Tip' );
 
-  QUnit.module( 'Lu Tips' );
+  $.when.apply( $, [ mouseTipComponent.deferral,
+                     inputTipComponent.deferral,
+                     persistentTipComponent.deferral ] ).done( function () {
 
-  QUnit.asyncTest( 'Mouse triggered tip.' , function() {
-    mouseTipComponent.ready(function() {
+    QUnit.module( 'API Tests' );
+
+    test( 'Programmatic show', 1, function() {
+      mouseTipComponent.instance.show();
+      if( $( '.mouse-tip' ).is( ':visible' ) ) {
+        ok( true, 'Tip is shown' );
+      }
+    } );
+
+    test( 'Programmatic getPosition', 1, function() {
+      var position = mouseTipComponent.instance.getPosition();
+
+      if( position.top && position.left ) {
+        ok( true, 'Postion object has correct attributes' );
+      }
+    } );
+
+    asyncTest( 'Programmatic hide', 1, function() {
+      mouseTipComponent.instance.hide();
+      setTimeout( function() {
+        if( !$( '.mouse-tip' ).is( ':visible' ) ) {
+          ok( true, 'Tip is hidden' );
+          start();
+        }
+      }, 500 );
+    } );
+
+    QUnit.module( 'Interaction Tests' );
+
+    test( 'Mouse over show', 2, function() {
       mouseTipEl.trigger( 'mouseenter' );
-      expect( 2 );
-      ok( $.trim( $( '.mouse-tip' ).html() ) === 'Fatback pork belly flank salami cow t-bone ground round pancetta short ribs jerky pig pork sausage.', 'Tip content is correct.' );
-      start();
-      $(document).trigger( 'mousemove' );
-      stop();
-      setTimeout(function() {
-        ok( $( '.mouse-tip' ).css('display') === 'none', 'Tip has been hidden.' );
-        start();
-      }, 300);
-    });
-  });
+      ok( $.trim( $( '.mouse-tip' ).html() ) === 'Mouse tooltip content.', 'Tip content is correct.' );
+      if( $( '.mouse-tip' ).is( ':visible' ) ) {
+        ok( true, 'Tip is shown' );
+      }
+    } );
 
-  QUnit.asyncTest( 'Focus triggered tip.' , function() {
-    inputTipComponent.ready(function() {
+    asyncTest( 'Mouse leave hide', 1, function() {
+      $( document ).trigger( 'mousemove' );
+      setTimeout( function() {
+        if( !$( '.mouse-tip' ).is( ':visible' ) ) {
+          ok( true, 'Tip is hidden' );
+          start();
+        }
+      }, 500 );
+    } );
+
+    test( 'Focus show', 2, function() {
       inputTipEl.trigger( 'focus' );
-      expect( 2 );
-      ok( $.trim( $( '.input-tip' ).html() ) === 'Pastrami boudin pig, swine filet mignon venison flank chuck.', 'Tip content is correct.' );
-      start();
-      inputTipEl.trigger( 'blur' );
-      stop();
-      setTimeout(function() {
-        ok( $( '.input-tip' ).css( 'display' ) === 'none', 'Tip has been hidden.' );
-        start();
-      }, 300);
-    });
-  });
+      ok( $.trim( $( '.input-tip' ).html() ) === 'Input tooltip content.', 'Tip content is correct.' );
+      if( $( '.input-tip' ).is( ':visible' ) ) {
+        ok( true, 'Tip is shown' );
+      }
+    } );
 
+    asyncTest( 'Blur hide', 1, function() {
+      $( inputTipEl ).trigger( 'blur' );
+      setTimeout( function() {
+        if( !$( '.input-tip' ).is( ':visible' ) ) {
+          ok( true, 'Tip is hidden' );
+          start();
+        }
+      }, 500 );
+    } );
+
+    test( 'Show persistent tip', 1, function() {
+      persistentTipEl.trigger( 'mouseenter' );
+      if( $( '.persistent-tip' ).is( ':visible' ) ) {
+        ok( true, 'Tip is shown' );
+      }
+    } );
+
+    test( 'Hide persistent tip (with close button)', 1, function() {
+      $( '.persistent-tip .close' ).trigger( 'click' );
+      if( !$( '.persistent-tip' ).is( ':visible' ) ) {
+        ok( true, 'Tip is hidden' );
+      }
+    } );
+
+  } );
 }
